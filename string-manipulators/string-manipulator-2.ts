1e0ec52d3dbcde5e05041fd8ddcc4dc825ad638e
JGuo99 <jguo0310@gmail.com>
2020-07-05 17:09:02 -0400

    Updated Manip2

--- string-manipulators/string-manipulator-2.ts
+++ string-manipulators/string-manipulator-2.ts
@@ -25,7 +25,7 @@ export class StringManipulator2 extends StringManipulator {
     public manipulateString(str: string): string {
         let letterNumber = str.replace(/\a\ /g, 'u');
         letterNumber = letterNumber.replace(/\e\ /g, 'o');
-        letterNumber = letterNumber.replace(/\i\ /g, 'i');
+        letterNumber = letterNumber.replace(/\i\ /g, '1');
         letterNumber = letterNumber.replace(/\o\ /g, 'e');
         letterNumber = letterNumber.replace(/\u\ /g, 'a');
         
