707ad90aaef2dc7cab67a704cae8e6efc122c07d
fdemp <dempseyfrederick@gmail.com>
2020-07-05 15:54:25 -0500

    complete-string-manipulator-9

--- string-manipulators/string-manipulator-9.ts
+++ string-manipulators/string-manipulator-9.ts
@@ -8,7 +8,7 @@ export class StringManipulator9 extends StringManipulator {
 
     constructor() {
         super();
-        this.author = 'N/A';
+        this.author = 'Frederick Dempsey';
     }
 
     private tempHelperMethod(str: string): string {
@@ -17,6 +17,13 @@ export class StringManipulator9 extends StringManipulator {
 
     public manipulateString(str: string): string {
         let someVariable = this.tempHelperMethod(str);
+
+        /*Make the string more UwU*/
+        someVariable = someVariable.replace(/r/gi, "w");
+        someVariable = someVariable.replace(/l/gi, "w");
+        someVariable = someVariable.replace(/th /gi, "ff");
+        someVariable = someVariable.replace(/ th/gi, "d");
+        someVariable += "UwU";
         return someVariable;
     }
 }
